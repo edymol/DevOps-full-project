8adf129db40862c6ab5b14b0b22c2d5faabe64a6
Edy Molina <edymolinap@gmail.com>
2024-10-15 09:02:54 +0100

    add: @EnableFeignClients annotation on application

--- restaurant-menu-management-service/src/main/java/com/digiaxix/restaurantmenumanagementservice/RestaurantMenuManagementServiceApplication.java
+++ restaurant-menu-management-service/src/main/java/com/digiaxix/restaurantmenumanagementservice/RestaurantMenuManagementServiceApplication.java
@@ -3,9 +3,11 @@ package com.digiaxix.restaurantmenumanagementservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class RestaurantMenuManagementServiceApplication {
 
 	public static void main(String[] args) {
--- restaurant-order-management/src/main/java/com/digiaxix/restaurantordermanagement/RestaurantOrderManagementApplication.java
+++ restaurant-order-management/src/main/java/com/digiaxix/restaurantordermanagement/RestaurantOrderManagementApplication.java
@@ -2,9 +2,12 @@ package com.digiaxix.restaurantordermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class RestaurantOrderManagementApplication {
 
 	public static void main(String[] args) {
