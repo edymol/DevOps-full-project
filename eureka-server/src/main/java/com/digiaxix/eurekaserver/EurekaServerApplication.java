06c0b85bc162935efc5183327d1f35bca02c8f95
Edy Molina <edymolinap@gmail.com>
2024-12-26 11:59:17 +0000

    Updated .gitignore and cleaned unnecessary files

--- eureka-server/src/main/java/com/digiaxix/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/digiaxix/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.digiaxix.restaurant;
+package com.digiaxix.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class RestaurantApplication {
+public class EurekaServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RestaurantApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
 
 }
